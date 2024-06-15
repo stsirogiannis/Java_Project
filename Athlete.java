017921f0aef7fb51c8288cee35b1c700b71db929
errikaan <eanagnostopoulou60@gmail.com>
2024-06-15 14:23:43 +0300

    Add files via upload

---
+++ Athlete.java
@@ -0,0 +1,32 @@
+package e23178_e23009;
+
+
+public class Athlete extends User {                                  // η κλαση athlete κληρονομει την κλαση User
+	
+	private static boolean isProfessional;  //ιδιοτητα επαγγελματια, δηλωση των πεδιων της κλασης Athlete ως private static για να χρησιμοποιηθουν στην κλαση Subscription & Enrollement
+	private static int experience;         // χρονια εμπειριας                         
+	Athlete (int userCount, String name, String surname, char gender, String birthDate, String contactInfo, boolean isProfessional, int experience){
+		super (userCount, name, surname, gender, birthDate, contactInfo);
+		Athlete.setIsProfessional(isProfessional);
+		Athlete.setExperience(experience);
+	}
+	
+	public static void setIsProfessional(boolean isProfessional) {                       //ορισμος μεθοδων getters και setters για τη θεση και την ανακτηση των τιμων της Athlete
+		Athlete.isProfessional= isProfessional;
+	}
+	public static  boolean getIsProfessional() {
+		return Athlete.isProfessional;
+	}
+	
+	public static void setExperience(int experience) {
+		if (experience>=1 && experience<=5) {
+			Athlete.experience=experience;
+		}
+	}
+	public static int getExperience() {
+		return Athlete.experience;
+	}
+	
+	
+	
+}
